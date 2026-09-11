915bab82c8ace470aca797ef1b1acc7e5c42566f
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2026-09-11 13:34:24 -0400

    add svg animation of me

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,26 @@ function App() {
 
         {/* main page will be here */}
         <div className="main-content">
-          <p>{"so our current page is --> " + currentPage}</p>
+          {console.log("so our current page is --> " + currentPage)}
+
+          {/* workin on project page now... GULP! */}
+          <div className="projects">
+            {/* information about me */}
+            <div className="hero-row">
+              <div className="info-column">
+                <h1 className="main-font">Hello! I'm Sam,</h1>
+                <h2 className="main-font">
+                  A developer who translates user needs into universal design
+                </h2>
+
+                <p>Ex-Product Designer & SWE Intern at Limbitless Solutions</p>
+              </div>
+
+              {/* image of me  */}
+
+              <img src="animations/ANIMATION_1.svg"></img>
+            </div>
+          </div>
         </div>
       </div>
     </>
