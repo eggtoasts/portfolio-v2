11ea7ecaf7348d721d3bf01e9c15fdd2388866a6
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2026-09-11 11:11:27 -0400

    page state logic, fix sidebar shrinking

--- src/App.jsx
+++ src/App.jsx
@@ -12,45 +12,74 @@ function App() {
 
   return (
     <>
-      {/* sidebar here fer now... */}
-      <div className="sidebar">
-        {/* my name */}
-        <div className="name-row">
-          <img src="/sams_icon.svg" alt="Sam's logo" className="name-icon" />
-          <p className="main-font name">Sam Amaro</p>
-        </div>
+      <div className="app-layout">
+        {/* sidebar here fer now... */}
+        <div className="sidebar">
+          {/* my name */}
+          <div className="name-row">
+            <img src="/sams_icon.svg" alt="Sam's logo" className="name-icon" />
+            <p className="main-font name">Sam Amaro</p>
+          </div>
 
-        {/* navigation goes here */}
-        <div className="explore">
-          <p className="sub-font">EXPLORE</p>
+          {/* navigation goes here */}
+          <div className="explore">
+            <p className="sub-font">EXPLORE</p>
 
-          <div className="sidebar-nav main-font">
-            <button
-              className={`${currentPage == "Projects" ? "selected" : ""}`}
-            >
-              Projects
-            </button>
-            <button>About</button>
-            <button>Playground</button>
-            <button>Resume</button>
+            <div className="sidebar-nav main-font">
+              <button
+                onClick={() => setCurrentPage("Projects")}
+                className={`${currentPage == "Projects" ? "selected" : ""}`}
+              >
+                Projects
+              </button>
+              <button
+                onClick={() => setCurrentPage("About")}
+                className={`${currentPage == "About" ? "selected" : ""}`}
+              >
+                About
+              </button>
+              <button
+                onClick={() => setCurrentPage("Playground")}
+                className={`${currentPage == "Playground" ? "selected" : ""}`}
+              >
+                Playground
+              </button>
+              <button
+                onClick={() => setCurrentPage("Resume")}
+                className={`${currentPage == "Resume" ? "selected" : ""}`}
+              >
+                Resume
+              </button>
+            </div>
+          </div>
+
+          {/* links with my socials */}
+          <div className="bottom-socials">
+            <p className="sub-font">SOCIALS</p>
+
+            <div className="socials-row">
+              <div className="social">
+                <p className="sub-font">LINKEDIN</p>
+                <img
+                  src="/ix_arrow-diagonal-bottom-left.svg"
+                  alt="left arrow"
+                />
+              </div>
+
+              <div className="social">
+                <p className="sub-font">GITHUB</p>
+                <img
+                  src="/ix_arrow-diagonal-bottom-left.svg"
+                  alt="left arrow"
+                />
+              </div>
+            </div>
           </div>
         </div>
 
-        {/* links with my socials */}
-        <div className="bottom-socials">
-          <p className="sub-font">SOCIALS</p>
-
-          <div className="socials-row">
-            <div className="social">
-              <p className="sub-font">LINKEDIN</p>
-              <img src="/ix_arrow-diagonal-bottom-left.svg" alt="left arrow" />
-            </div>
-
-            <div className="social">
-              <p className="sub-font">GITHUB</p>
-              <img src="/ix_arrow-diagonal-bottom-left.svg" alt="left arrow" />
-            </div>
-          </div>
+        {/* main page will be here */}
+        <div className="main-content">
+          <p>{"so our current page is --> " + currentPage}</p>
         </div>
       </div>
     </>
