e92ee6c9703ab010435592d180eebb3aa3153ee7
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2026-09-12 19:45:19 -0400

    internship page text block styles

--- src/pages/LimbitlessPage.jsx
+++ src/pages/LimbitlessPage.jsx
@@ -6,6 +6,7 @@ export default function LimbitlessPage() {
         <p className="sub-font">INTERNSHIP</p>
         <h1 className="main-font">Limbitless Solutions</h1>
 
+        {/* info abt limb, should  make it into a component  in the future for other pages*/}
         <div className="info-row">
           <p className="info-col sub-font">
             <p>ROLE</p>
@@ -41,6 +42,28 @@ export default function LimbitlessPage() {
               <p>Jan 2026 - Current</p>
             </div>
           </p>
+        </div>
+
+        {/* mission */}
+
+        <div className="paragraph">
+          <div className="title-block">
+            <p className="sub-header">THE MISSION</p>
+            <h2 className="header">
+              <span className="blue">Empowering</span> children with limb
+              differences through expressive and affordable 3D-printed bionic
+              arms.
+            </h2>
+          </div>
+
+          <div className="text-block">
+            <p>
+              Limbitless Solutions was my first ever tech internship, and the
+              place where I learned to be a designer and an engineer at the same
+              time. I started on the web team, coding designs and learning Figma
+              along the way, before moving over to the CS side.
+            </p>
+          </div>
         </div>
       </div>
     </>
