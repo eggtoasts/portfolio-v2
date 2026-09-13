0039486eb358248871ed87083ed755dfa9130675
eggtoasts <129806552+eggtoasts@users.noreply.github.com>
2026-09-13 19:28:46 -0400

    setup my projects page

--- src/pages/MyProjects/MyProjects.jsx
+++ src/pages/MyProjects/MyProjects.jsx
@@ -1,5 +1,36 @@
 import { useState } from "react";
 import "./MyProjects.css";
+import Paragraph from "../../components/Paragraph";
 export default function MyProjects() {
-  return <div className="page"></div>;
+  return (
+    <>
+      {/* banner! */}
+      <img
+        className="project-banner"
+        src="/project-banners/EventKnightPlaceholder.svg"
+      ></img>
+
+      <div className="page">
+        <p className="sub-font">LEARNING</p>
+        <h1 className="main-font main-title">I’m Always Learning!</h1>
+
+        <div id="context">
+          <Paragraph
+            subheader={"THE CONTEXT"}
+            header={""}
+            arr={["Projects throughout my learning journey."]}
+            color={""}
+            coloredText={""}
+          />
+        </div>
+
+        {/* projects i've worked on */}
+
+        <div className="project-container">
+          {/* project 1 */}
+          <div id="project-01"></div>
+        </div>
+      </div>
+    </>
+  );
 }
